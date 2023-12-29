c99ec72af4993350130e01df03e997ae90e81088
jarp <r@geeks.ms>
2023-12-29 02:44:26 -0300

    data folders

--- init.sh
+++ init.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+
+# Define local folders
+data_folders=("mdb1-data" "redis-data" "mysql-data")
+
+# Create folder if folder doesn't exist
+for folder in "data/${data_folders[@]}"; do
+    if [ ! -d "./$folder" ]; then
+        mkdir "./$folder"
+        echo "Creada la carpeta $folder"
+    else
+        echo "La carpeta $folder ya existe, no es necesario crearla."
+    fi
+done
+
+# Run docker compose
 docker-compose --env-file .env up -d
